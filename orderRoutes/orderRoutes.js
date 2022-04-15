74b21059c836b2ccb20f4100d03583c458c1fc23
Genesis <genesisybenitez1@gmail.com>
2022-04-14 23:34:44 -0400

    backend final

--- orderRoutes/orderRoutes.js
+++ orderRoutes/orderRoutes.js
@@ -1,46 +1,55 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const db = require("../mysql/dbConfig");
 
-router.get("/getAllOrders", (request,response) =>{
-    db.query("select * from orders", (err,results) =>{
-            if(err) throw err;
-            response.send(results);
-        })
-    
-});
-
-router.get("/getOrdersByUserId/:user_id", (request,response) =>{
-    db.query(`
-    select orders.created_at, orders.id, orders.total, orders.quantity,
-products.name,products.id AS product_id, products.price, products.product_img,
-user_payment.id, user_payment.payment_type, user_payment.account_number,
-users.first_name, users.last_name
-from orders 
-inner join products on orders.product_id = products.id
-inner join user_payment on orders.user_payment_id = user_payment.id
-inner join users on orders.user_id = users.id where orders.user_id = ?`,[request.params.user_id], (err,results) =>{
+router.get("/getAllOrders", (request, response)=>{
+    db.query("select * from orders", (err, results)=>{
         if(err) throw err;
         response.send(results);
     })
-});
+})
 
+router.get("/getOrdersByUserId/:user_id", (request, response)=>{
+    db.query(`select orders.created_at, orders.id AS orderId, orders.total, orders.quantity, 
+    products.name, products.id AS product_id, products.price, products.product_img,
+    user_payment.id, user_payment.payment_type, user_payment.account_number ,
+    users.first_name, users.last_name
+    from orders 
+    inner join products on orders.product_id = products.id
+    inner join user_payment on orders.user_payment_id = user_payment.id
+    inner join users on orders.user_id = users.id where orders.user_id = ? order by orders.created_at DESC`, [request.params.user_id], (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+    })
+})
+
+router.post("/getOrdersByUserIdFilter/:user_id", (request, response)=>{
+    db.query(`select orders.created_at, orders.id AS orderId, orders.total, orders.quantity, 
+    products.name, products.id AS product_id, products.price, products.product_img,
+    user_payment.id, user_payment.payment_type, user_payment.account_number ,
+    users.first_name, users.last_name
+    from orders 
+    inner join products on orders.product_id = products.id
+    inner join user_payment on orders.user_payment_id = user_payment.id
+    inner join users on orders.user_id = users.id where orders.user_id = ? AND date(orders.created_at) = ?`, [request.params.user_id, request.body.date], (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+        console.log(results)
+    })
+})
 
 router.post("/addOrder", (request, response)=>{
-            db.query(`insert into orders(user_id,product_id,total,quantity, user_payment_id)values (?, ?, ?,?,?)`, [request.body.user_id,request.body.product_id,request.body.total,request.body.quantity, request.body.user_payment_id ], (err, results)=>{
-                if(err) throw err;
-               
-            })
-            db.query(`update products set quantity = quantity - ? where id = ?`, [request.body.quantity,request.body.product_id], (err, results)=>{
-                if(err) throw err;
-                
-            })
-            db.query(`delete from cart where user_id = ?`, [request.body.user_id], (err, results)=>{
-                if(err) throw err;
-               
-            })
-            response.send("Order successfully added, Product quantity successfully updated, Cart item successfully removed");
+    db.query(`insert into orders(user_id, product_id, total, quantity, user_payment_id) values (?, ?, ?, ?, ?)`, [request.body.user_id,request.body.product_id,request.body.total,request.body.quantity ,request.body.user_payment_id ], (err, results)=>{
+        if(err) throw err;
+    })
+    db.query(`update products set quantity = quantity - ? where id = ?`, [request.body.quantity,request.body.product_id ], (err, results)=>{
+        if(err) throw err;
+    })
+    db.query(`delete from cart where user_id = ?`, [request.body.user_id], (err, results)=>{
+        if(err) throw err;
+    })
+    response.send("Order successfully added, Product quantity successfully updated, cart items successfully removed");
+
 })
 
 
--- productRoutes/productRoutes.js
+++ productRoutes/productRoutes.js
@@ -1,72 +1,75 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const db = require("../mysql/dbConfig");
 
-router.get("/getAllProducts", (request,response) =>{
-    db.query("select * from products", (err,results) =>{
-            if(err) throw err;
-            response.send(results);
-        })
-    
-});
-
-router.get("/getAllProductCategories", (request,response) =>{
-    db.query("select * from product_category", (err,results) =>{
-            if(err) throw err;
-            response.send(results);
-        })
-    
-});
-
-router.get("/getProduct/:id", (request,response) =>{
-    db.query("select * from products where id = ?",[request.params.id], (err,results) =>{
+router.get("/getAllProducts", (request, response)=>{
+    db.query("select * from products", (err, results)=>{
         if(err) throw err;
         response.send(results);
     })
-});
+})
 
-router.post("/addProduct", (request,response)=>{
-    db.query(`insert into products(name,description,category_id,price, quantity, product_img)values(?,?,?,?,?,?)`, [request.body.name,request.body.description, request.body.category_id,request.body.price,request.body.quantity,request.body.product_img], (err,results) =>{
+router.get("/getAllProductCategories", (request, response)=>{
+    db.query("select * from product_category", (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+    })
+})
+
+router.get("/getProduct/:id", (request, response)=>{
+    db.query(`select products.id, products.name, products.description, products.category_id, 
+    products.price, products.quantity, products.product_img, product_category.name AS category
+    from products 
+    inner join product_category on products.category_id = product_category.id where products.id = ?`, [request.params.id], (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+    })
+})
+
+router.post("/addProduct", (request, response)=>{
+    db.query(`insert into products(name, description, category_id, price, quantity, product_img)values (?, ?, ?, ?, ? , ?)`, [request.body.name,request.body.description,request.body.category_id,request.body.price, request.body.quantity,request.body.product_img ], (err, results)=>{
         if(err) throw err;
         response.send("Product successfully added");
     })
-});
-
-//join queries
-router.get("/getAllProductInformation", (request,response) =>{
-
-db.query(`select products.id ,products.name, products.description, products.category_id, 
-products.price, products.quantity, products.product_img, product_category.name AS category
-from products 
-inner join product_category on products.category_id = product_category.id`, (err,results) =>{
-    if(err) throw err;
-    response.send({username: request.session.username, userId: request.session.userId,products: results});
-
-}) 
-})
-router.get('/getProductCategories', (request,response)=>{
-    db.query(`select * from product_category`, (err,results) =>{
-        if(err) throw err;
-        response.send(results);
-    
-    })  
-    
-
 })
 
-router.post('/addProductCategory', (request,response)=>{
-    db.query(`insert into product_category(name,description)values(?,?)`,[request.body.name,request.body.description], (err,results) =>{
+router.post("/addProductCategory", (request, response)=>{
+    db.query(`insert into product_category(name, description)values (?, ?)`, [request.body.name,request.body.description ], (err, results)=>{
         if(err) throw err;
         response.send("Product Category successfully added");
-    
-    })  
-});
-router.get("/getProductsByCategory/:id", (request,response) =>{
-    db.query("select * from products where category_id = ?",[request.params.id], (err,results) =>{
-            if(err) throw err;
-            response.send(results);
-        })
-    
-});
+    })
+})
+
+
+//join queries
+router.get("/getAllProductInformation", (request, response)=>{
+    db.query(`select products.id, products.name, products.description, products.category_id, 
+    products.price, products.quantity, products.product_img, product_category.name AS category
+    from products 
+    inner join product_category on products.category_id = product_category.id`, (err, results)=>{
+        if(err) throw err;
+        response.send({username: request.session.username, userId: request.session.userId,products: results});
+})
+})
+
+router.get('/getProductCategories',(request, response)=>{
+    db.query(`select * from product_category`, (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+})
+})
+
+router.get('/getProductsByCategory/:id',(request, response)=>{
+    db.query(`select * from products where category_id = ?`,[request.params.id], (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+})
+})
+
+router.get('/getProductsByPrice/:id',(request, response)=>{
+    db.query(`select * from products where price < ?`,[request.params.id], (err, results)=>{
+        if(err) throw err;
+        response.send(results);
+})
+})
 module.exports = router;
